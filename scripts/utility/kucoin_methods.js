6cf914d7c0cdac7bc1a6841e217a9834268eec37
Matthew Ehinger <mnehinger@gmail.com>
2018-01-15 21:24:32 -0600

    kucoin methods

--- scripts/utility/kucoin_methods.js
+++ scripts/utility/kucoin_methods.js
@@ -5,19 +5,47 @@ var secret = require("../../secrets/secret_kucoin.json");
 
 let kc = new Kucoin(secret.key, secret.secret);
 
+
+//getBalances();
+
 function getBalances() {
     return new Promise((resolve, reject) => {
         kc.getBalance().then(result => {
+            console.log(result);
             resolve(result);
         }).catch(err => {
             reject(err);
         });
     })
 }
+//
+// kc.getOrderBooks({
+//     pair: 'POWR-BTC'
+// }).then(result => {
+//
+//     const data = result.data;
+//     const sell = data.SELL;
+//     const buy = data.BUY;
+//     console.log("data: ", data);
+//
+//     }).catch(console.error);
+//
 
-kc.getOrderBooks({
-    pair: 'POWR-BTC'
-}).then(console.log).catch(console.error);
+calculateSpread();
+
+
+function calculateSpread(baseProduct) {
+
+    kc.getExchangeRates().then(result => {
+        const data = result.data;
+        const currencies = data.currencies;
+        console.log("currencies: ", currencies);
+        const rates = data.rates;
+        console.log("rates: ", rates);
+    }).catch(console.error)
+
+}
+
 
 
 function buy(args) {
@@ -31,8 +59,3 @@ function buy(args) {
 }
 
 
-// kc.getExchangeRates({symbols: ['NEO','GAS']}).then(result => {
-//     console.log("result.data: ", result.data);
-// }).catch(console.error);
-//
-// 
